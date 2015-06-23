095a820ecabb51ccd29ca0bb71d77c85e9619678
Adrián Pérez <adrian@simbiotica.es>
2015-06-23 11:59:14 +0200

    Test tests, badmtss

--- lib/gfw_api_client/data_set.rb
+++ lib/gfw_api_client/data_set.rb
@@ -1,6 +1,24 @@
 module GfwApiClient
 	class DataSet
-		# Code
+	    def forest_change type, data_set, params=nil, *path_args
+	      url = construct_url_root(type,data_set)
+	      url += "/#{path_args.join('/')}"
+	      url += "?#{params.to_query}"
+	      self.response = Typhoeus.get(url, followlocation: true)
+	    end
+		private
+		    def construct_url_root type, data_set
+		      if type.to_sym == :iso
+		        "#{base_url}/forest-change/#{table_name(data_set)}/admin"
+		      elsif type.to_sym == :ifl
+		        "#{base_url}/forest-change/#{table_name(data_set)}/admin/#{type.to_s}"
+		      elsif type.to_sym == :wdpa || type.to_sym == :use
+		        "#{base_url}/forest-change/#{table_name(data_set)}/#{type.to_s}"
+		      end
+		    end
+		    def base_url
+		      @base_url ||= GfwApiClient.config['base_url']
+		    end
+		end
 	end
 end
-
