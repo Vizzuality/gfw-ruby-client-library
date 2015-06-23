29bf712a2ec4f1698923b638053f9c5bada773e2
rschumann <sebastian.schkudlara@gmail.com>
2015-06-23 12:34:34 +0200

    Test response

--- lib/gfw_api_client.rb
+++ lib/gfw_api_client.rb
@@ -4,7 +4,9 @@ require 'gfw_api_client/version'
 require 'gfw_api_client/data_set'
 
 module GfwApiClient
-  # Your code goes here...
+
+  def self.find_set(data_set, iso)
+    DataSet.find_imazon(data_set, iso)
+  end
+
 end
-
-
--- lib/gfw_api_client/data_set.rb
+++ lib/gfw_api_client/data_set.rb
@@ -1,6 +1,7 @@
-module GfwApiClient
-	class DataSet
-		# Code
-	end
-end
+class DataSet
 
+  def self.find_imazon(data_set, iso)
+  	Typhoeus.get("http://staging.gfw-apis.appspot.com/forest-change/#{data_set}/admin/#{iso}")
+  end
+
+end
