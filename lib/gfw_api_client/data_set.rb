0153af5de849a785ec180a095cc1372108eea6b7
Adrián Pérez <adrian@simbiotica.es>
2015-06-24 11:16:15 +0200

    Imazon geojson support

--- lib/gfw_api_client/data_set.rb
+++ lib/gfw_api_client/data_set.rb
@@ -1,15 +1,14 @@
 class DataSet
-
   def self.find_imazon(data_set, type, params)
   	case type
   	when 'iso'
 		Typhoeus.get("http://staging.gfw-apis.appspot.com/forest-change/#{data_set}/admin/#{params}")
 	when 'wdpa'
-		puts params
 		Typhoeus.get("http://staging.gfw-apis.appspot.com/forest-change/#{data_set}/wdpa/#{params}")
 	when 'use'
-		puts params
 		Typhoeus.get("http://staging.gfw-apis.appspot.com/forest-change/#{data_set}/use/#{JSON.parse(params)['type']}/#{JSON.parse(params)['id']}")
+	when 'geojson'
+		Typhoeus.get("http://staging.gfw-apis.appspot.com/forest-change/#{data_set}?type=geojson&geojson=#{CGI::escape(params)}")
 	end
   end
 
